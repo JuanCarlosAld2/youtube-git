4bed3177e88057439b920a994010aaccaca2bda2
Juan Carlos <jcaldama94@gmail.com>
2023-09-12 10:59:30 -0600

    first commit

--- Apuntes.js
+++ Apuntes.js
@@ -1,11 +1,70 @@
 /*
 TODOS LOS COMANDOS SE ECUENTRA EN LA PAGINA DE JON MIRCHA
+ COMANDOS QUE SE ME HACEN MAS IMPORTANTES 
 
 --global => instalacion para todo el sistema opertaivo
 
 git config --list => "listar las configuracion global"
 
-git config --global -e =>  "abrir configuracion en "
+git config --global -e =>  "abrir configuracion en VSC"
+
+git config --global core.autocrlf true 
+
+    - para estandarizar los saltos de línea en windows 
+    -sirve para que no encuentre problemas a la hora de trabajar con alguien de linux o ios
+
+git help config 
+    - abre la documentacion en el navegador aun sin tener internet 
+
+mkdir nombre-de-la-carpeta 
+    -Crear una carpeta
+
+
+archivo redme.md 
+    -explicacion y documentacion de cada proyecto libreria, fremwork, 
+    -crear un archivo touch README.md
+
+touch .gitignore => crear un archivo .gitignore para ignorar archivos que le sean señalados
+
+git init - inicializar un proyecto por primera ves y solo se jecuta 1 vez
+
+FLUJO BASICO DE GIT 
+    -Consta de 4 estados 
+        - 3 de ellos son locales.
+        - 1 mas en GitHub o plataforma 
+
+    -Estados
+        modified = modificado        
+        staged = estar traqueado por git 
+        commited =  la cabeza ya tiene los cambios localmente
+        remote = se sube el codigo a la plataforma 
+
+    working Directory 
+        - directorio de trabajao 
+        - es cualquier carpeta 
+
+    git pull => descaragar del remoto al  direcotrio de trabajo las actualizaciones 
+
+    revisar carpeta img flujo git.jpg
+
+    git commit => modificas el archivo "COMMIT_EDITMSG" en vsc 
+    
+
+
+
+
+
+
+*/
+
+
+/*
+COMANDOS DE CONSOLA 
+
+ls -a = mostrara todos los archivos hasta los ocultos
+
+
+
 
 
 */
