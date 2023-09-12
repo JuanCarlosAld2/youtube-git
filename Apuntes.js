da0a182347e8a0f7e5a15068b4d405dfbeb17169
Juan Carlos <jcaldama94@gmail.com>
2023-09-12 10:45:42 -0600

    Primercommit👌

---
+++ Apuntes.js
@@ -0,0 +1,11 @@
+/*
+TODOS LOS COMANDOS SE ECUENTRA EN LA PAGINA DE JON MIRCHA
+
+--global => instalacion para todo el sistema opertaivo
+
+git config --list => "listar las configuracion global"
+
+git config --global -e =>  "abrir configuracion en "
+
+
+*/
